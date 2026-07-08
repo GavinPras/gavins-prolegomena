ed5469558e739a80caec02cfe05ab66d0926a32a
GavinPras <136899300+GavinPras@users.noreply.github.com>
2026-07-08 21:32:47 +0700

    new ui

--- src/data/catalog.js
+++ src/data/catalog.js
@@ -7,7 +7,7 @@ export const CATALOG = [
     title: "Why Love Cannot Be Defined by Love Itself ", 
     excerptHtml: "Philosophical reflections on <em>love</em>.",
     date: "Jun 30, 2026",
-    cover: "public/images/why-love/cover.png",
+    cover: "/images/why-love/cover.png",
     coverPosition: "left"
   },
   {
@@ -18,7 +18,7 @@ export const CATALOG = [
     title: "Why Do We Always Generalize Things?", 
     excerptHtml: 'Philosophical ideas of <em>Category Intersections</em>.', 
     date: "Jun 26, 2026",
-    cover: "public/images/categories-intersections/cover.png",
+    cover: "/images/categories-intersections/cover.png",
     coverPosition: "left"
   },
   {
@@ -29,7 +29,7 @@ export const CATALOG = [
     title: "A Beautiful Terrifying Dream",
     excerptHtml: 'Philosophical interpretations of the anime <em>Paprika</em>.',
     date: "Mar 1, 2026",
-    cover: "public/images/paprika-a-beautiful-terrifying-dream/cover.png"
+    cover: "/images/paprika-a-beautiful-terrifying-dream/cover.png"
   },
   {
     slug: "analyzing-fullmetal-alchemist",
@@ -39,7 +39,7 @@ export const CATALOG = [
     title: "Analyzing the Logic of the Law of Equivalent Exchange",
     excerptHtml: 'Philosophical ideas about the law of equivalent exchange in the anime <em>Fullmetal Alchemist: Brotherhood</em>.',
     date: "Feb 14, 2026",
-    cover: "public/images/analyzing-fullmetal-alchemist/cover.png"
+    cover: "/images/analyzing-fullmetal-alchemist/cover.png"
   },
   {
     slug: "hachiman-existentialism",
@@ -49,7 +49,7 @@ export const CATALOG = [
     title: "Hachiman as a Genuine Expression of Existentialism",
     excerptHtml: 'Philosophical reflections on the anime <em>Oregairu</em>.',
     date: "Feb 9, 2026",
-    cover: "public/images/hachiman-as-a-genuine-exp-of-ex/cover.png"
+    cover: "/images/hachiman-as-a-genuine-exp-of-ex/cover.png"
   },
   {
     slug: "what-if-life-could-be-sold",
@@ -60,7 +60,7 @@ export const CATALOG = [
     excerptHtml:
       'Philosophical interpretations of the manga <em>I Sold My Life for Ten Thousand Yen per Year</em>.',
     date: "Dec 7, 2025",
-    cover: "public/images/what-if-life-could-be-sold/cover.png"
+    cover: "/images/what-if-life-could-be-sold/cover.png"
   },
 ];
 
