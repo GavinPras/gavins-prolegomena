05421a4c89c37a441c0ff36e56ced52f395b3a7a
GavinPras <136899300+GavinPras@users.noreply.github.com>
2026-07-10 22:46:45 +0700

    new ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 import IndexPage from "./pages/IndexPage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
 import EssayPage from "./pages/EssayPage.jsx";
 
 export default function App() {
   return (
+    <>    
+    <ScrollToTop />
     <Routes>
       <Route path="/" element={<IndexPage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/essay" element={<EssayPage />} />
-      {}
       <Route path="*" element={<IndexPage />} />
     </Routes>
+    </>
+
   );
 }
